f9ecbddd258cc8e137142f71394b4fb3b4136fc0
ernesto905 <ernesto90643@gmail.com>
2025-12-01 18:40:31 -0500

    wrapped up day 1

--- 2025/day1/SecretEntrance.java
+++ 2025/day1/SecretEntrance.java
@@ -38,12 +38,73 @@ public class SecretEntrance {
 
     return password;
   }
-}
 
-public static void main(String[] args) {
-  File partOneFile = new File("part1.input");
-  int password = SecretEntrance.partOne(partOneFile);
+  private static int partTwo(File file) {
+    int password = 0;
+    int dialNumber = 50;
 
-  System.out.println("Password: " + password);
+    try {
+      Scanner input = new Scanner(file);
 
+      while (input.hasNextLine()) {
+        String rotation = input.nextLine();
+
+        int rotationAmount = Integer.parseInt(rotation.replaceAll("[^0-9]", ""));
+        char rotationDirection = rotation.charAt(0);
+
+        if (rotationDirection == 'L') {
+          int oldDialNumber = dialNumber;
+
+          while (rotationAmount > oldDialNumber) {
+            // start from 0 & never loop around
+            if (oldDialNumber == 0 && rotationAmount < 100) {
+              break;
+            }
+
+            rotationAmount = rotationAmount - 100;
+            password = password + 1;
+          }
+
+          dialNumber = Math.floorMod(dialNumber - rotationAmount, 100);
+        } else {
+
+          while (rotationAmount > 100) {
+            rotationAmount = rotationAmount - 100;
+            password = password + 1;
+          }
+
+          if (rotationAmount + dialNumber > 100) {
+            rotationAmount = rotationAmount - 100;
+            password = password + 1;
+          }
+
+          dialNumber = Math.floorMod(dialNumber + rotationAmount, 100);
+        }
+
+        if (dialNumber == 0) {
+          password = password + 1;
+        }
+
+      }
+
+      input.close();
+
+    } catch (FileNotFoundException fnfe) {
+      System.out.println("add a part2.input file");
+    }
+
+    return password;
+  }
+
+  public static void main(String[] args) {
+    File partOneFile = new File("part1.input");
+    File partTwoFile = new File("part2.input");
+
+    int partOnePassword = SecretEntrance.partOne(partOneFile);
+    int partTwoPassword = SecretEntrance.partTwo(partTwoFile);
+
+    System.out.println("Part 1 pw: " + partOnePassword);
+    System.out.println("Part 2 pw: " + partTwoPassword);
+
+  }
 }
